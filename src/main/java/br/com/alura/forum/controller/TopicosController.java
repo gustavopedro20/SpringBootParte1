192e404e6c7028011f84f67fb00661782cb155ec
Gustavo <gustavo.goncalves@fncit.com.br>
2020-01-29 09:31:10 -0300

    add param para filtrar topicos por nome do curso

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,16 +1,13 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import br.com.alura.forum.controller.dto.TopicoDTO;
 import br.com.alura.forum.repository.TopicoRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import br.com.alura.forum.controller.dto.TopicoDTO;
-import br.com.alura.forum.modelo.Curso;
-import br.com.alura.forum.modelo.Topico;
 
 @RestController
 public class TopicosController {
@@ -19,8 +16,12 @@ public class TopicosController {
     private TopicoRepository topicoRepository;
 
     @RequestMapping("/topicos")
-    public List<TopicoDTO> lista() {
-        return TopicoDTO.converter(topicoRepository.findAll());
+    public List<TopicoDTO> lista(String nomeCurso) {
+        if (nomeCurso == null) {
+            return TopicoDTO.converter(topicoRepository.findAll());
+        } else {
+            return TopicoDTO.converter(topicoRepository.findByCursoNome(nomeCurso));
+        }
     }
 
 }
